f77f7a49948c6c212c4637a2e23e109d32bb312c
raw34 <raw34@sina.com>
2021-12-10 11:20:07 +0800

    update

---
+++ 1206/SkipList.go
@@ -0,0 +1,19 @@
+package _1206
+
+type Skiplist struct {
+}
+
+func Constructor() Skiplist {
+    return Skiplist{}
+}
+
+func (this *Skiplist) Search(target int) bool {
+    return true
+}
+
+func (this *Skiplist) Add(num int) {
+}
+
+func (this *Skiplist) Erase(num int) bool {
+    return true
+}
