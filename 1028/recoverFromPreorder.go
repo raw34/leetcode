7088ca89eaa1fa58db1b0b9ed1ad41ceed10f078
raw34 <raw34@sina.com>
2021-12-15 12:50:51 +0800

    update

--- 1028/recoverFromPreorder.go
+++ 1028/recoverFromPreorder.go
@@ -22,16 +22,16 @@ func recoverFromPreorder(traversal string) *TreeNode {
     hasVal := false
     for i := 0; i < len(traversal); i++ {
         curr := traversal[i]
+        if curr == '-' && hasVal {
+            addNode(dep, val)
+            dep = 0
+            val = 0
+            hasVal = false
+        }
         if curr != '-' {
             val = val*10 + int(curr-'0')
             hasVal = true
         } else {
-            if hasVal {
-                addNode(dep, val)
-                dep = 0
-                val = 0
-                hasVal = false
-            }
             dep++
         }
     }
