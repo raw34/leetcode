8e13134992aeafe7e8a8fc78806650d5730db847
raw34 <raw34@sina.com>
2021-12-16 16:47:39 +0800

    update

--- 1206/SkipList.go
+++ 1206/SkipList.go
@@ -1,19 +1,98 @@
 package _1206
 
+import "math/rand"
+
+const MaxLevel = 32
+const P = 0.25
+
+type SkiplistNode struct {
+    Val  int
+    Next []*SkiplistNode
+}
+
+func NewSkiplistNode(val, level int) *SkiplistNode {
+    return &SkiplistNode{Val: val, Next: make([]*SkiplistNode, level)}
+}
+
 type Skiplist struct {
+    level int
+    head  *SkiplistNode
 }
 
 func Constructor() Skiplist {
-    return Skiplist{}
+    return Skiplist{level: 1, head: NewSkiplistNode(-1, MaxLevel)}
 }
 
 func (this *Skiplist) Search(target int) bool {
-    return true
+    prevNode := this.head
+    // 逐层遍历，寻找前序节点
+    for i := this.level - 1; i >= 0; i-- {
+        prevNode = this.findPrevNode(prevNode, i, target)
+        if prevNode.Next[i] != nil && prevNode.Next[i].Val == target {
+            return true
+        }
+    }
+
+    return false
 }
 
+func (this *Skiplist) findPrevNode(node *SkiplistNode, level, val int) *SkiplistNode {
+    // 当前节点有后序节点，且节点值小于目标值，从下个节点找起
+    for node.Next[level] != nil && node.Next[level].Val < val {
+        node = node.Next[level]
+    }
+
+    return node
+}
 func (this *Skiplist) Add(num int) {
+    level := this.randomLevel()
+    newNode := NewSkiplistNode(num, level)
+    if level > this.level {
+        // 如果随机层比当前最大层大
+        // 将新节点更新到每层第一个节点后
+        for i := this.level; i < level; i++ {
+            this.head.Next[i] = newNode
+        }
+        this.level = level
+    } else {
+        // 如果随机层比当前最大层小
+        // 逐层遍历，寻找前序节点
+        prevNode := this.head
+        for i := level - 1; i >= 0; i-- {
+            prevNode = this.findPrevNode(prevNode, i, num)
+            if prevNode.Next[i] == nil {
+                // 最近节点无后序节点，直接更新到该节点后
+                prevNode.Next[i] = newNode
+            } else {
+                // 最近节点有后序节点，插入当前节点后，并将后序节点后移
+                prevNode.Next[i], newNode.Next[i] = newNode, prevNode.Next[i]
+            }
+        }
+    }
+}
+
+func (this *Skiplist) randomLevel() int {
+    level := 1
+    for rand.Float64() < P && level < MaxLevel {
+        level++
+    }
+
+    return level
 }
 
 func (this *Skiplist) Erase(num int) bool {
-    return true
+    res := false
+    prevNode := this.head
+    // 逐层遍历，寻找前序节点
+    for i := this.level - 1; i >= 0; i-- {
+        prevNode = this.findPrevNode(prevNode, i, num)
+        // 当找到前序节点，且其后序节点为目标值，删除节点，后序节点前移
+        if prevNode.Next[i] != nil && prevNode.Next[i].Val == num {
+            prevNode.Next[i] = prevNode.Next[i].Next[i]
+            res = true
+            continue
+        }
+    }
+
+    return res
 }
---
+++ 1206/SkipList_test.go
@@ -0,0 +1,38 @@
+package _1206
+
+import (
+    "github.com/stretchr/testify/assert"
+    "testing"
+)
+
+//["Skiplist", "add", "add", "add", "search","add", "search", "erase", "erase", "search"]
+//[[], [1], [2], [3], [0], [4], [1], [0], [1], [1]]
+//[null, null, null, null, false,null, true, false, true, false]
+func TestSkiplist_Case1(t *testing.T) {
+    list := Constructor()
+    list.Add(1)
+    list.Add(2)
+    list.Add(3)
+    assert.False(t, list.Search(0))
+    list.Add(4)
+    assert.True(t, list.Search(1))
+    assert.False(t, list.Erase(0))
+    assert.True(t, list.Erase(1))
+    assert.False(t, list.Search(1))
+}
+
+//["Skiplist", "add", "add", "add", "add","search", "erase", "search", "search", "search"]
+//[[], [0], [5], [2], [1], [0], [5], [2], [3], [2]]
+//[null,null,null,null,null,true,true,true,false,true]
+func TestSkiplist_Case2(t *testing.T) {
+    list := Constructor()
+    list.Add(0)
+    list.Add(5)
+    list.Add(2)
+    list.Add(1)
+    assert.True(t, list.Search(0))
+    assert.True(t, list.Erase(5))
+    assert.True(t, list.Search(2))
+    assert.False(t, list.Search(3))
+    assert.True(t, list.Search(2))
+}
