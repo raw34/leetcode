84baa41939cf5389f10e7288a3f529806b9500f9
raw34 <raw34@sina.com>
2021-12-17 13:37:48 +0800

    update

--- runtime/MaxPriorityQueue.go
+++ runtime/MaxPriorityQueue.go
@@ -4,13 +4,12 @@ import "fmt"
 
 type MaxPriorityQueue struct {
     queue []int
-    size  int
 }
 
 func NewMaxPriorityQueue(nums []int) MaxPriorityQueue {
-    queue := make([]int, len(nums)+1)
+    queue := make([]int, 1)
     queue[0] = -1
-    maxPQ := MaxPriorityQueue{queue, 0}
+    maxPQ := MaxPriorityQueue{queue}
     for i := 0; i < len(nums); i++ {
         maxPQ.Push(nums[i])
     }
@@ -22,9 +21,8 @@ func (this *MaxPriorityQueue) less(i, j int) bool {
 }
 
 func (this *MaxPriorityQueue) Push(val int) {
-    this.size++
-    this.queue[this.size] = val
-    this.swim(this.size)
+    this.queue = append(this.queue, val)
+    this.swim(len(this.queue) - 1)
 }
 
 func (this *MaxPriorityQueue) swim(k int) {
@@ -40,18 +38,17 @@ func (this *MaxPriorityQueue) swap(i, j int) {
 
 func (this *MaxPriorityQueue) Pop() int {
     max := this.queue[1]
-    this.swap(1, this.size)
-    this.queue = this.queue[0:this.size]
-    this.size--
+    this.swap(1, len(this.queue)-1)
+    this.queue = this.queue[:len(this.queue)-1]
     this.sink(1)
     return max
 }
 
 func (this *MaxPriorityQueue) sink(k int) {
-    for this.left(k) <= this.size {
+    for this.left(k) <= len(this.queue) {
         olderChild := this.left(k)
         rightChild := this.right(k)
-        if rightChild <= this.size && this.less(olderChild, rightChild) {
+        if rightChild <= len(this.queue) && this.less(olderChild, rightChild) {
             olderChild = rightChild
         }
 
