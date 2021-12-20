573af84a6de6cf65306634eebc574e32c596c7d8
raw34 <raw34@sina.com>
2021-12-20 12:31:07 +0800

    update

--- 0113/pathSum.go
+++ 0113/pathSum.go
@@ -5,19 +5,19 @@ import "github.com/raw34/leetcode/runtime"
 func pathSum(root *runtime.TreeNode, targetSum int) [][]int {
     res := make([][]int, 0)
     path := make([]int, 0)
-    var dfs func(root *runtime.TreeNode, path []int, num int)
-    dfs = func(root *runtime.TreeNode, path []int, num int) {
+    var dfs func(root *runtime.TreeNode, path []int, sum int)
+    dfs = func(root *runtime.TreeNode, path []int, sum int) {
         if root == nil {
             return
         }
-        num += root.Val
+        sum += root.Val
         path = append(path, root.Val)
-        if root.Left == nil && root.Right == nil && num == targetSum {
+        if root.Left == nil && root.Right == nil && sum == targetSum {
             res = append(res, append([]int{}, path...))
             return
         }
-        dfs(root.Left, path, num)
-        dfs(root.Right, path, num)
+        dfs(root.Left, path, sum)
+        dfs(root.Right, path, sum)
         path = path[:len(path)-1]
     }
     dfs(root, path, 0)
