514ae23281c7eaa23e2a92fd73dae0ae688b24e0
raw34 <raw34@sina.com>
2021-12-22 13:49:17 +0800

    update

---
+++ 0079/exist.go
@@ -0,0 +1,36 @@
+package _0079
+
+func exist(board [][]byte, word string) bool {
+    m := len(board)
+    n := len(board[0])
+    used := make([][]bool, m)
+    for i := 0; i < m; i++ {
+        used[i] = make([]bool, n)
+    }
+    var check func(row, col, i int) bool
+    check = func(row, col, i int) bool {
+        if i == len(word) {
+            return true
+        }
+        if row < 0 || col < 0 || row >= m || col >= n || used[row][col] || board[row][col] != word[i] {
+            return false
+        }
+
+        used[row][col] = true
+        if check(row+1, col, i+1) || check(row-1, col, i+1) || check(row, col+1, i+1) || check(row, col-1, i+1) {
+            return true
+        }
+        used[row][col] = false
+        return false
+    }
+
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            if check(i, j, 0) {
+                return true
+            }
+        }
+    }
+
+    return false
+}
