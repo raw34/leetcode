63fe77f94d4020c3e8811927b00631fd751e97a4
raw34 <raw34@sina.com>
2021-12-23 20:40:13 +0800

    update

--- 1863/subsetXORSum.go
+++ 1863/subsetXORSum.go
@@ -3,17 +3,16 @@ package _1863
 func subsetXORSum(nums []int) int {
     length := len(nums)
     res := 0
-    path := 0
-    var backtrack func(begin int)
-    backtrack = func(begin int) {
+    var backtrack func(path, begin int)
+    backtrack = func(path, begin int) {
         res += path
         for i := begin; i < length; i++ {
             path ^= nums[i]
-            backtrack(i + 1)
+            backtrack(path, i+1)
             path ^= nums[i]
         }
     }
-    backtrack(0)
+    backtrack(0, 0)
 
     return res
 }
