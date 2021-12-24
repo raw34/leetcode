f37e3e7cfca1b7240fa3668ba832e1be54a00dcf
raw34 <raw34@sina.com>
2021-12-24 16:48:30 +0800

    update

--- 0673/findNumberOfLIS.go
+++ 0673/findNumberOfLIS.go
@@ -7,7 +7,9 @@ func findNumberOfLIS(nums []int) int {
         }
         return a
     }
-    maxLen := 1
+
+    res := 0
+    maxLen := 0
     n := len(nums)
     dp := make([]int, n)
     cnt := make([]int, n)
@@ -16,22 +18,21 @@ func findNumberOfLIS(nums []int) int {
         cnt[i] = 1
         for j := 0; j < i; j++ {
             if nums[j] < nums[i] {
-                if dp[i] < dp[j]+1 {
+                if dp[i] == dp[j]+1 {
+                    cnt[i] += cnt[j]
+                } else if dp[i] < dp[j]+1 {
                     dp[i] = dp[j] + 1
                     cnt[i] = cnt[j]
-                } else if dp[i] == dp[j]+1 {
-                    cnt[i] += cnt[j]
                 }
             }
         }
-        maxLen = max(maxLen, dp[i])
-    }
-
-    res := 0
-    for i := 0; i < n; i++ {
         if dp[i] == maxLen {
             res += cnt[i]
+        } else if dp[i] > maxLen {
+            maxLen = dp[i]
+            res = cnt[i]
         }
+        maxLen = max(maxLen, dp[i])
     }
 
     return res
