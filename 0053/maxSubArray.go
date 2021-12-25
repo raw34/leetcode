c2122be110c46098aa2c5c7a81585e8ddce31976
raw34 <raw34@sina.com>
2021-12-25 13:03:50 +0800

    update

--- 0053/maxSubArray.go
+++ 0053/maxSubArray.go
@@ -9,6 +9,29 @@ func maxSubArray(nums []int) int {
     }
 
     n := len(nums)
+    dp := make([]int, n)
+    dp[0] = nums[0]
+    for i := 1; i < n; i++ {
+        dp[i] = max(nums[i], dp[i-1]+nums[i])
+    }
+
+    res := nums[0]
+    for i := 0; i < n; i++ {
+        res = max(res, dp[i])
+    }
+
+    return res
+}
+
+func maxSubArray2(nums []int) int {
+    max := func(a, b int) int {
+        if a < b {
+            return b
+        }
+        return a
+    }
+
+    n := len(nums)
     res := nums[0]
     // dp[i]表示到索引i为止的最大子序列和
     dp := make([]int, n)
