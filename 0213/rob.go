9e8d4c680ef000da304881bd4f633793e99294d9
raw34 <raw34@sina.com>
2021-12-26 14:27:18 +0800

    update

--- 0213/rob.go
+++ 0213/rob.go
@@ -19,7 +19,7 @@ func rob(nums []int) int {
     // dp[i]表示到当前家为止最大的收益
     dp := make([]int, n)
 
-    // 第一种情况，偷第一家
+    // 第一种情况，偷第一家，不偷最后一家
     dp[0] = nums[0]
     dp[1] = nums[0]
     max1 := nums[0]
@@ -28,7 +28,7 @@ func rob(nums []int) int {
         max1 = max(max1, dp[i])
     }
 
-    // 第一种情况，不偷第一家
+    // 第一种情况，不偷第一家，偷最后一家
     dp[0] = 0
     dp[1] = nums[1]
     max2 := nums[1]
@@ -36,6 +36,37 @@ func rob(nums []int) int {
         dp[i] = max(dp[i-1], dp[i-2]+nums[i])
         max2 = max(max2, dp[i])
     }
+
+    return max(max1, max2)
+}
+
+func rob2(nums []int) int {
+    n := len(nums)
+    if n == 0 {
+        return 0
+    }
+    if n == 1 {
+        return nums[0]
+    }
+
+    max := func(a, b int) int {
+        if a < b {
+            return b
+        }
+        return a
+    }
+
+    robMax := func(start, end int) int {
+        prevMax := 0
+        currMax := 0
+        for i := start; i < end; i++ {
+            prevMax, currMax = currMax, max(currMax, prevMax+nums[i])
+        }
+        return currMax
+    }
+
+    max1 := robMax(0, n-1)
+    max2 := robMax(1, n)
 
     return max(max1, max2)
 }
