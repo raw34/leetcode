3061ae634603f019e6ce557427c3abd3f0bd2285
raw34 <raw34@sina.com>
2021-12-26 11:27:15 +0800

    update

---
+++ 0198/rob.go
@@ -0,0 +1,25 @@
+package _0198
+
+func rob(nums []int) int {
+    max := func(a, b int) int {
+        if a < b {
+            return b
+        }
+        return a
+    }
+
+    res := nums[0]
+    n := len(nums)
+    dp := make([]int, n)
+    dp[0] = nums[0]
+    for i := 1; i < n; i++ {
+        if i == 1 {
+            dp[i] = max(dp[i-1], nums[i])
+        } else {
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+        }
+        res = max(res, dp[i])
+    }
+
+    return res
+}
