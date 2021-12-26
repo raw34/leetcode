577b4616c4ff8f8513d70e27be454972f32be67e
raw34 <raw34@sina.com>
2021-12-26 20:05:26 +0800

    update

--- 0873/lenLongestFibSubseq.go
+++ 0873/lenLongestFibSubseq.go
@@ -17,20 +17,20 @@ func lenLongestFibSubseq(arr []int) int {
     // dp定义：从索引i到j的符合条件子序列个数
     dp := make([][]int, n)
     res := 0
-    for i := 0; i < n; i++ {
-        dp[i] = make([]int, n)
-        for j := i + 1; j < n; j++ {
+    for curr := 0; curr < n; curr++ {
+        dp[curr] = make([]int, n)
+        for next := curr + 1; next < n; next++ {
             // 任意两个数都可以作为子序列的初始值
-            dp[i][j] = 2
-            diff := arr[j] - arr[i]
+            dp[curr][next] = 2
+            diff := arr[next] - arr[curr]
             // 寻找当前两数的前序数字
-            k, ok := index[diff]
+            prev, ok := index[diff]
             // 如果前序数字存在，且数字小于当前第一位数字
-            if arr[k] < arr[i] && ok {
+            if arr[prev] < arr[curr] && ok {
                 // 更新当前序列长度
-                dp[i][j] = dp[k][i] + 1
+                dp[curr][next] = dp[prev][curr] + 1
                 // 更新全局最长子序列长度
-                res = max(res, dp[i][j])
+                res = max(res, dp[curr][next])
             }
         }
     }
