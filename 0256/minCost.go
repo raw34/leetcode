29fa9a0c68a36883256acd235f7d10d517d06070
raw34 <raw34@sina.com>
2021-12-28 19:02:50 +0800

    update

--- 0256/minCost.go
+++ 0256/minCost.go
@@ -12,11 +12,14 @@ func minCost(costs [][]int) int {
     }
 
     n := len(costs)
+    // dp[i][j]表示第i个房子用j颜色时的最小花费
     dp := make([][3]int, n)
     dp[0][0] = costs[0][0]
     dp[0][1] = costs[0][1]
     dp[0][2] = costs[0][2]
+    // 逐个房子遍历，每次计算当前房子的消费
     for i := 1; i < n; i++ {
+        // 计算时考虑不连续同色情况
         dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + costs[i][0]
         dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + costs[i][1]
         dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + costs[i][2]
