b83342e063acabd3b0f9735d70638e990f062174
raw34 <raw34@sina.com>
2021-12-28 10:42:00 +0800

    update

--- 0091/numDecodedings.go
+++ 0091/numDecodedings.go
@@ -5,9 +5,11 @@ func numDecodings(s string) int {
     dp := make([]int, n+1)
     dp[0] = 1
     for i := 1; i <= n; i++ {
+        // 前一位数字不为0，初始化当前位解法数为前一位解法数
         if s[i-1] != '0' {
-            dp[i] += dp[i-1]
+            dp[i] = dp[i-1]
         }
+        // 当前一位为两位数时，继续累加前前一位的解法数
         if i > 1 && s[i-2] != '0' && ((s[i-2]-'0')*10+(s[i-1]-'0')) <= 26 {
             dp[i] += dp[i-2]
         }
