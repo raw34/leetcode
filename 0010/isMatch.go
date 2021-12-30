0522a64481e3d7e1de92c4c697b96cb6d92ec92a
raw34 <raw34@sina.com>
2021-12-30 21:14:58 +0800

    update

--- 0010/isMatch.go
+++ 0010/isMatch.go
@@ -7,7 +7,9 @@ func isMatch(s string, p string) bool {
     for i := 0; i < m+1; i++ {
         dp[i] = make([]bool, n+1)
     }
+    // 初始化第一个位置
     dp[0][0] = true
+    // 初始化第一列，如果遇到*且前一个位置匹配， 则下一个位置赋值为匹配
     for j := 0; j < n; j++ {
         if p[j] == '*' && dp[0][j-1] {
             dp[0][j+1] = true
@@ -17,12 +19,10 @@ func isMatch(s string, p string) bool {
         for j := 1; j < n+1; j++ {
             if s[i-1] == p[j-1] || p[j-1] == '.' {
                 dp[i][j] = dp[i-1][j-1]
+            } else if p[j-1] == '*' && s[i-1] != p[j-2] && p[j-2] != '.' {
+                dp[i][j] = dp[i][j-2]
             } else if p[j-1] == '*' {
-                if s[i-1] != p[j-2] && p[j-2] != '.' {
-                    dp[i][j] = dp[i][j-2]
-                } else {
-                    dp[i][j] = dp[i-1][j]
-                }
+                dp[i][j] = dp[i][j-1] || dp[i][j-2] || dp[i-1][j]
             }
         }
     }
