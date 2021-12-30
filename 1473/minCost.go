a0d8ba632b1922faaac50a0b1f93efe16cdfd621
raw34 <raw34@sina.com>
2021-12-30 15:16:36 +0800

    update

--- 1473/minCost.go
+++ 1473/minCost.go
@@ -16,17 +16,22 @@ func minCost(houses []int, cost [][]int, m int, n int, target int) int {
         return res
     }
 
+    // dp[i][j][k] 表示第i个房子在第j个街区刷第k种颜色时的最小花费
     dp := make([][][]int, m)
+    // 初始化操作
     for i := 0; i < m; i++ {
         dp[i] = make([][]int, target+1)
         for j := 0; j < target+1; j++ {
             dp[i][j] = make([]int, n+1)
             for k := 0; k < n+1; k++ {
                 if i == 0 && j == 1 && k == houses[0] && houses[0] > 0 {
+                    // 第1个房子第1个街区的情况下，如果第1个房子已有颜色，花费为0
                     dp[i][j][k] = 0
                 } else if i == 0 && j == 1 && k > 0 && houses[0] == 0 {
+                    // 第1个房子第1个街区的情况下，如果第1个房子没有颜色，花费为第1个房子当前颜色的金额
                     dp[i][j][k] = cost[i][k-1]
                 } else {
+                    // 其他情况全都初始化为不可用金额
                     dp[i][j][k] = math.MaxInt32
                 }
             }
@@ -35,14 +40,20 @@ func minCost(houses []int, cost [][]int, m int, n int, target int) int {
 
     for i := 1; i < m; i++ {
         for j := 1; j < target+1; j++ {
-            color := houses[i]
+            k := houses[i]
             if houses[i] > 0 {
-                dp[i][j][color] = min(-1, dp[i][j][color], dp[i-1][j][color])
-                dp[i][j][color] = min(-1, dp[i][j][color], min(color, dp[i-1][j-1]...))
+                // 当前房子已有颜色
+                // 初始化当前花费为上一个房子为止的花费，因为当前房子不用刷
+                dp[i][j][k] = dp[i-1][j][k]
+                // 获取变街区和不变街区的花费，取最小值
+                dp[i][j][k] = min(-1, dp[i][j][k], min(k, dp[i-1][j-1]...))
             } else {
-                for color = 1; color < n+1; color++ {
-                    dp[i][j][color] = min(-1, dp[i][j][color], dp[i-1][j][color]+cost[i][color-1])
-                    dp[i][j][color] = min(-1, dp[i][j][color], min(color, dp[i-1][j-1]...)+cost[i][color-1])
+                // 当前房子没有颜色，遍历每种颜色，分别计算花费
+                for k = 1; k < n+1; k++ {
+                    // 初始化当前花费为上一个房子为止的花费 + 当前房子的花费
+                    dp[i][j][k] = dp[i-1][j][k] + cost[i][k-1]
+                    // 获取变街区和不变街区的花费，取最小值
+                    dp[i][j][k] = min(-1, dp[i][j][k], min(k, dp[i-1][j-1]...)+cost[i][k-1])
                 }
             }
         }
