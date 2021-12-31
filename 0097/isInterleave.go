699ae018fd6d87dc2aa1a4a6e0060dfa1da6308a
raw34 <raw34@sina.com>
2021-12-31 15:32:54 +0800

    update

--- 0097/isInterleave.go
+++ 0097/isInterleave.go
@@ -7,34 +7,32 @@ func isInterleave(s1 string, s2 string, s3 string) bool {
         return false
     }
 
+    // dp[i][j]表示s1前i个元素和s2前j个元素能否交错组成s3前i+j个元素
     dp := make([][]bool, m+1)
     for i := 0; i < m+1; i++ {
         dp[i] = make([]bool, n+1)
     }
+    // 初始化s1和s2都不取的情况
     dp[0][0] = true
-    for i := 0; i < m+1; i++ {
-        for j := 0; j < n+1; j++ {
+    // 初始化第一列，当s3前i个字符都由s1组成的情况
+    for i := 1; i < m+1 && s1[i-1] == s3[i-1]; i++ {
+        dp[i][0] = true
+    }
+    // 初始化第一行，当s3前j个字符都由s2组成的情况
+    for j := 1; j < n+1 && s2[j-1] == s3[j-1]; j++ {
+        dp[0][j] = true
+    }
+    // 从第一个字符开始遍历，逐个字符比较计算
+    for i := 1; i < m+1; i++ {
+        for j := 1; j < n+1; j++ {
             k := i + j
-            //a := s1[i-1]
-            //b := s2[j-1]
-            //c1 := s3[k-1]
-            //c2 := s3[k]
-            //fmt.Println(i, j, k, string(a), string(b), string(c1), string(c2))
-            //if a == c2 && b == c1 || a == c1 && b == c2 {
-            //    dp[i][j] = dp[i-1][j-1]
-            //} else if a != c1 && a != c2 && (b == c1 || b == c2) {
-            //    dp[i][j] = dp[i-1][j]
-            //} else if b != c1 && b != c2 && (a == c1 || a == c2) {
-            //    dp[i][j] = dp[i][j-1]
-            //} else {
-            //    dp[i][j] = dp[i][j-1] || dp[i-1][j] || dp[i-1][j-1]
-            //}
-            if i > 0 {
-                dp[i][j] = dp[i][j] || dp[i-1][j] && s1[i-1] == s3[k-1]
-            }
-            if j > 0 {
-                dp[i][j] = dp[i][j] || dp[i][j-1] && s2[j-1] == s3[k-1]
-            }
+            c1 := s1[i-1]
+            c2 := s2[j-1]
+            c3 := s3[k-1]
+            // 有两种情况算匹配
+            // 第一种，前一行匹配且s1当前字符与s3当前字符相同
+            // 第二种，前一列匹配且s2当前字符与s3当前字符相同
+            dp[i][j] = dp[i-1][j] && c1 == c3 || dp[i][j-1] && c2 == c3
         }
     }
 
