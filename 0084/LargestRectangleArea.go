8720be9d463cec76f48b65fba12171887247793f
raw34 <raw34@sina.com>
2022-01-04 17:45:20 +0800

    update

--- 0084/LargestRectangleArea.go
+++ 0084/LargestRectangleArea.go
@@ -1,24 +1,27 @@
 package _084
 
-import (
-	"github.com/raw34/leetcode/runtime"
-)
-
 func largestRectangleArea(heights []int) int {
+	max := func(a, b int) int {
+		if a < b {
+			return b
+		}
+		return a
+	}
+
 	area := 0
 	stack := make([]int, 0)
 	newHeights := append([]int{0}, append(heights, 0)...)
 	for i := 0; i < len(newHeights); i++ {
-		for len(stack) > 0 && newHeights[i] < newHeights[stack[len(stack)-1]]{
+		for len(stack) > 0 && newHeights[i] < newHeights[stack[len(stack)-1]] {
 			curr := stack[len(stack)-1]
-			stack = stack[0:len(stack)-1]
+			stack = stack[0 : len(stack)-1]
 			currH := newHeights[curr]
 
 			left := stack[len(stack)-1]
 			right := i
 			currW := right - left - 1
 
-			area = runtime.Max(area, currW * currH)
+			area = max(area, currW*currH)
 		}
 
 		stack = append(stack, i)
