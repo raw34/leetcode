bbbb7a9a03c6dca489891da7a3786b4a36f64df8
raw34 <raw34@sina.com>
2022-01-12 22:44:13 +0800

    update

--- 0146/LRUCache.go
+++ 0146/LRUCache.go
@@ -19,14 +19,22 @@ func (this *LRUCache) Get(key int) int {
         // 未命中缓存，直接返回
         return -1
     }
+    value := this.hash[key]
+    // 将元素重新缓存
+    this.Put(key, value)
+    // 返回元素
+    return value
+}
+
+func (this *LRUCache) Put(key int, value int) {
     // 清理缓存
     this.cleanup(key)
-    // 返回查询元素
-    return this.hash[key]
+    // 插入新元素
+    this.hash[key] = value
 }
 
 func (this *LRUCache) cleanup(key int) {
-    if this.hash[key] > 0 {
+    if _, ok := this.hash[key]; ok {
         // 缓存命中，找到并删除命中的key
         for i := 0; i < len(this.queue); i++ {
             if this.queue[i] == key {
@@ -41,13 +49,6 @@ func (this *LRUCache) cleanup(key int) {
         // 清理被出队的key的缓存
         delete(this.hash, keyOld)
     }
-    // 将新元素入队
+    // 将元素key入队
     this.queue = append(this.queue, key)
-}
-
-func (this *LRUCache) Put(key int, value int) {
-    // 清理缓存
-    this.cleanup(key)
-    // 插入新元素
-    this.hash[key] = value
 }
