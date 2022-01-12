cc420348570d74c85a7ed0f22ac68828114948eb
raw34 <raw34@sina.com>
2022-01-12 13:57:07 +0800

    update

--- 0239/maxSlidingWindow.go
+++ 0239/maxSlidingWindow.go
@@ -2,21 +2,21 @@ package _0239
 
 func maxSlidingWindow(nums []int, k int) []int {
     n := len(nums)
-    // 利用单调队列将数组中k位置前单调递增的数字入队
     queue := make([]int, 0)
-    for i := 0; i < k; i++ {
+    enqueue := func(i int) {
         for len(queue) > 0 && nums[i] >= nums[queue[len(queue)-1]] {
             queue = queue[:len(queue)-1]
         }
         queue = append(queue, i)
     }
+    // 利用单调队列将数组中k位置前单调递增的数字入队
+    for i := 0; i < k; i++ {
+        enqueue(i)
+    }
     // 从k开始寻找每个滑动窗口的最大值
     res := []int{nums[queue[0]]}
     for i := k; i < n; i++ {
-        for len(queue) > 0 && nums[i] >= nums[queue[len(queue)-1]] {
-            queue = queue[:len(queue)-1]
-        }
-        queue = append(queue, i)
+        enqueue(i)
         for queue[0] <= i-k {
             queue = queue[1:]
         }
