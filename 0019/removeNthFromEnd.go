90ccc6dd5518fcf24ce3bc31576cd692599edad2
raw34 <raw34@sina.com>
2022-01-12 17:34:37 +0800

    update

--- 0019/removeNthFromEnd.go
+++ 0019/removeNthFromEnd.go
@@ -3,6 +3,7 @@ package _0019
 import "github.com/raw34/leetcode/runtime"
 
 func removeNthFromEnd(head *runtime.ListNode, n int) *runtime.ListNode {
+    // 将链表存到map里，以便利用索引寻找节点
     hash := map[int]*runtime.ListNode{}
     dummy := &runtime.ListNode{Next: head}
     curr := dummy
@@ -13,6 +14,7 @@ func removeNthFromEnd(head *runtime.ListNode, n int) *runtime.ListNode {
         i++
     }
 
+    // 从map中寻找并删除节点
     hash[i-n-1].Next = hash[i-n+1]
 
     return dummy.Next
