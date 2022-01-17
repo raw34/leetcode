83663182be77c2c353fed8ce4c6ccaa9b8982c8b
raw34 <raw34@sina.com>
2022-01-18 07:53:49 +0800

    update

--- 0025/reverseKGroup.go
+++ 0025/reverseKGroup.go
@@ -3,35 +3,39 @@ package _0025
 import "github.com/raw34/leetcode/runtime"
 
 func reverseKGroup(head *runtime.ListNode, k int) *runtime.ListNode {
-    reverse := func(head *runtime.ListNode) *runtime.ListNode {
-        var prev *runtime.ListNode
-        curr := head
-        for curr != nil {
+    reverse := func(head *runtime.ListNode, left, right int) *runtime.ListNode {
+        dummy := &runtime.ListNode{Next: head}
+
+        prev := dummy
+        for i := 0; i < left-1; i++ {
+            prev = prev.Next
+        }
+
+        curr := prev.Next
+        for i := 0; i < right-left; i++ {
             next := curr.Next
-            curr.Next = prev
-            prev = curr
-            curr = next
+            curr.Next = next.Next
+            next.Next = prev.Next
+            prev.Next = next
         }
-        return prev
+
+        return dummy.Next
     }
 
-    dummy := &runtime.ListNode{Next: head}
-    pre, end := dummy, dummy
-    for end.Next != nil {
-        for i := 0; i < k && end != nil; i++ {
-            end = end.Next
+    // 求链表长度
+    curr := head
+    n := 0
+    for ; curr != nil; n++ {
+        curr = curr.Next
+    }
+    // 循环调用反转链表II的方法
+    curr = head
+    for l, r := 1, k; r < n+1; r++ {
+        if r%k == 0 {
+            head = reverse(head, l, r)
+            l = r + 1
         }
-        if end == nil {
-            break
-        }
-        start := pre.Next
-        next := end.Next
-        end.Next = nil
-        pre.Next = reverse(start)
-        start.Next = next
-        pre = start
-        end = pre
     }
 
-    return dummy.Next
+    return head
 }
