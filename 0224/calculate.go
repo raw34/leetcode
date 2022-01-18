b1c55087f6573907cb388a411cf89ec9a5af61dd
raw34 <raw34@sina.com>
2022-01-18 23:29:41 +0800

    update

--- 0224/calculate.go
+++ 0224/calculate.go
@@ -9,28 +9,35 @@ func calculate(s string) int {
         s = "0" + s
     }
 
-    sum := func(left, opt, right string) string {
-        num1, _ := strconv.Atoi(left)
-        num2, _ := strconv.Atoi(right)
-        if opt == "-" {
-            num2 = -num2
-        }
-        return strconv.Itoa(num1 + num2)
-    }
-
     n := len(s)
     stack := make([]string, 0)
+
+    updateStack := func(right string) {
+        length := len(stack)
+        if length > 0 && (stack[length-1] == "+" || stack[length-1] == "-") && stack[length-2] != "(" {
+            opt := stack[length-1]
+            left := stack[length-2]
+            stack = stack[:length-2]
+            num1, _ := strconv.Atoi(left)
+            num2, _ := strconv.Atoi(right)
+            if opt == "-" {
+                num2 = -num2
+            }
+            sum := strconv.Itoa(num1 + num2)
+            stack = append(stack, sum)
+        } else {
+            stack = append(stack, right)
+        }
+    }
+
     for i := 0; i < n; i++ {
+        length := len(stack)
         curr := string(s[i])
         if curr == " " {
             continue
-        }
-        if curr == "+" || curr == "-" || curr == "(" {
+        } else if curr == "+" || curr == "-" || curr == "(" {
             stack = append(stack, curr)
-        }
-
-        length := len(stack)
-        if curr >= "0" && curr <= "9" {
+        } else if curr >= "0" && curr <= "9" {
             right := curr
             if length > 1 && stack[length-1] == "-" && stack[length-2] == "(" {
                 right = "-" + right
@@ -40,35 +47,18 @@ func calculate(s string) int {
                 right += string(s[i+1])
                 i++
             }
-            length = len(stack)
-            if length > 0 && (stack[length-1] == "+" || stack[length-1] == "-") && stack[length-2] != "(" {
-                opt := stack[length-1]
-                left := stack[length-2]
-                stack = stack[:length-2]
-                stack = append(stack, sum(left, opt, right))
-            } else {
-                stack = append(stack, right)
-            }
+            updateStack(right)
         } else if curr == ")" {
             right := stack[length-1]
             stack = stack[:length-1]
             length = len(stack)
-            //fmt.Println("case2", stack)
-            if length > 2 && stack[length-1] == "-" && stack[length-2] == "(" {
+            if length > 1 && stack[length-1] == "-" && stack[length-2] == "(" {
                 right = "-" + right
                 stack = stack[:length-2]
             } else {
                 stack = stack[:length-1]
             }
-            length = len(stack)
-            if length > 0 && (stack[length-1] == "+" || stack[length-1] == "-") && stack[length-2] != "(" {
-                opt := stack[length-1]
-                left := stack[length-2]
-                stack = stack[:length-2]
-                stack = append(stack, sum(left, opt, right))
-            } else {
-                stack = append(stack, right)
-            }
+            updateStack(right)
         }
     }
 
