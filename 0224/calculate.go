4e41953e15add7bba2298e108b4b239cdc205165
raw34 <raw34@sina.com>
2022-01-18 23:43:00 +0800

    update

--- 0224/calculate.go
+++ 0224/calculate.go
@@ -5,6 +5,7 @@ import (
 )
 
 func calculate(s string) int {
+    // 追加前导0，以方便后续统一处理逻辑
     if s[0] == '-' {
         s = "0" + s
     }
@@ -12,7 +13,8 @@ func calculate(s string) int {
     n := len(s)
     stack := make([]string, 0)
 
-    updateStack := func(right string) {
+    // 追加数字到栈顶，根据情况进行计算或直接入栈
+    appendNum := func(right string) {
         length := len(stack)
         if length > 0 && (stack[length-1] == "+" || stack[length-1] == "-") && stack[length-2] != "(" {
             opt := stack[length-1]
@@ -34,31 +36,41 @@ func calculate(s string) int {
         length := len(stack)
         curr := string(s[i])
         if curr == " " {
+            // 当前字符为空格，忽略
             continue
         } else if curr == "+" || curr == "-" || curr == "(" {
+            // 当前字符为 +  -  ( 直接入栈
             stack = append(stack, curr)
         } else if curr >= "0" && curr <= "9" {
+            // 当前字符为数字时
             right := curr
+            // 先处理前一个字符为 - 的情况
             if length > 1 && stack[length-1] == "-" && stack[length-2] == "(" {
                 right = "-" + right
                 stack = stack[:length-1]
             }
+            // 再处理当前数字为多位的情况
             for i+1 < n && s[i+1] >= '0' && s[i+1] <= '9' {
                 right += string(s[i+1])
                 i++
             }
-            updateStack(right)
+            // 调用函数，入栈
+            appendNum(right)
         } else if curr == ")" {
+            // 当前字符为 ） 时
+            // 先从栈顶拿到当前数字
             right := stack[length-1]
             stack = stack[:length-1]
             length = len(stack)
+            // 先处理前一个字符为 - 的情况，再移除最近一个 (
             if length > 1 && stack[length-1] == "-" && stack[length-2] == "(" {
                 right = "-" + right
                 stack = stack[:length-2]
             } else {
                 stack = stack[:length-1]
             }
-            updateStack(right)
+            // 调用函数，入栈
+            appendNum(right)
         }
     }
 
