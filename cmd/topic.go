6adb868906cbde9593858e82789bc03950c95c78
raw34 <raw34@sina.com>
2022-01-21 16:40:59 +0800

    update

--- cmd/topic.go
+++ cmd/topic.go
@@ -29,77 +29,167 @@ var topicCmd = &cobra.Command{
     Run: func(cmd *cobra.Command, args []string) {
         fmt.Println("topic called")
         name := cmd.Flag("name").Value.String()
-        fmt.Println("name is", name)
-        // 读取README文件（可选）
-        readmePath := "README.md"
-        content, err := ioutil.ReadFile(readmePath)
-        if err != nil {
-            panic(err)
-        }
-        // 解析README文件（可选）
-        markdown := goldmark.New()
-        readme := (markdown.Parser()).Parse(text.NewReader(content))
-        // 遍历README文件，获取已完成题目编号
-        readme = readme.FirstChild()
-        questionsDone := map[int]bool{}
-        for readme.NextSibling() != nil {
-            readme = readme.NextSibling()
-            kind := readme.Kind().String()
-            if kind == "Paragraph" {
-                child := readme.FirstChild().NextSibling()
-                for child.NextSibling() != nil {
-                    child = child.NextSibling()
-                    row := strings.Split(string(child.Text(content)), "|")
-                    done, no := strings.Trim(row[1], " "), strings.Trim(row[2], " ")
-                    if done == "✅" {
-                        i, _ := strconv.Atoi(no)
-                        questionsDone[i] = true
-                    }
-                }
-                break
-            }
-        }
-
-        // 读取全部题目文件
-        questionsPath := "questions.json"
-        content, err = ioutil.ReadFile(questionsPath)
-        if err != nil {
-            panic(err)
-        }
-        // 解析全部题目文件
-        questions := gjson.Parse(string(content)).Array()
-        // 创建标签文件
-        topicPath := "topic/" + strings.Replace(name, " ", "", -1) + ".md"
-        title := fmt.Sprintf("## %s\n", name)
-        title += "| No.  | Title                                                       | Mark |\n"
-        title += "|------|-------------------------------------------------------------|------|\n"
-        err = ioutil.WriteFile(topicPath, []byte(title), 0644)
-        if err != nil {
-            panic(err)
-        }
-        topicFile, err := os.OpenFile(topicPath, os.O_WRONLY|os.O_APPEND, 0644)
-        if err != nil {
-            panic(err)
-        }
-        defer topicFile.Close()
-        // 遍历全部题目，找到包含当前标签的题目，逐行写入文件
-        for _, question := range questions {
-            topics := question.Get("topicTags").Array()
-            for _, topic := range topics {
-                if topic.Get("name").String() == name {
-                    no := int(question.Get("questionId").Int())
-                    if !questionsDone[no] {
-                        continue
-                    }
-                    title := question.Get("title").String()
-                    row := fmt.Sprintf("| %d | %s | |\n", no, title)
-                    // TODO 格式化一下当前行，补若干个空格
-                    _, err = topicFile.WriteString(row)
-                    if err != nil {
-                        panic(err)
-                    }
-                }
-            }
-        }
+        // 全部题目
+        questions := getAllQuestions()
+        // 已完成的题目
+        doneQuestions := getDoneQuestions()
+        // 问题写入标签文件
+        question2Topic(name, questions, doneQuestions)
     },
+}
+
+func getAllQuestions() []gjson.Result {
+    // 读取全部题目文件
+    questionsPath := "questions.json"
+    content, err := ioutil.ReadFile(questionsPath)
+    if err != nil {
+        panic(err)
+    }
+    // 解析全部题目文件
+    questions := gjson.Parse(string(content)).Array()
+    return questions
+}
+
+func getDoneQuestions() map[int]bool {
+    doneQuestions := map[int]bool{}
+    // 读取README文件
+    readmePath := "README.md"
+    content, err := ioutil.ReadFile(readmePath)
+    if err != nil {
+        panic(err)
+    }
+    // 解析README文件
+    markdown := goldmark.New()
+    readme := (markdown.Parser()).Parse(text.NewReader(content))
+    // 遍历README文件，获取已完成题目编号
+    readme = readme.FirstChild()
+    for readme.NextSibling() != nil {
+        readme = readme.NextSibling()
+        kind := readme.Kind().String()
+        if kind == "Paragraph" {
+            child := readme.FirstChild().NextSibling()
+            for child.NextSibling() != nil {
+                child = child.NextSibling()
+                row := strings.Split(string(child.Text(content)), "|")
+                done, no := strings.Trim(row[1], " "), strings.Trim(row[2], " ")
+                if done == "✅" {
+                    i, _ := strconv.Atoi(no)
+                    doneQuestions[i] = true
+                }
+            }
+            break
+        }
+    }
+    return doneQuestions
+}
+
+func question2Topic(name string, questions []gjson.Result, doneQuestions map[int]bool) {
+    topicPath := "topic/" + strings.Replace(name, " ", "", -1) + ".md"
+    title := fmt.Sprintf("## %s\n", name)
+    title += "| No.  | Title                                                       | Mark |\n"
+    title += "|------|-------------------------------------------------------------|------|\n"
+    err := ioutil.WriteFile(topicPath, []byte(title), 0644)
+    if err != nil {
+        panic(err)
+    }
+    topicFile, err := os.OpenFile(topicPath, os.O_WRONLY|os.O_APPEND, 0644)
+    if err != nil {
+        panic(err)
+    }
+    defer topicFile.Close()
+    // 遍历全部题目，找到包含当前标签的题目，逐行写入文件
+    for _, question := range questions {
+        topics := question.Get("topicTags").Array()
+        for _, topic := range topics {
+            if topic.Get("name").String() == name {
+                no := int(question.Get("questionId").Int())
+                if !doneQuestions[no] {
+                    continue
+                }
+                title := question.Get("title").String()
+                row := fmt.Sprintf("| %d | %s | |\n", no, title)
+                // TODO 格式化一下当前行，补若干个空格
+                _, err = topicFile.WriteString(row)
+                if err != nil {
+                    panic(err)
+                }
+            }
+        }
+    }
+}
+
+func getAllTopics() []string {
+    return []string{
+        "Array",
+        "String",
+        "Hash Table",
+        "Dynamic Programming",
+        "Math",
+        "Depth-First Search",
+        "Sorting",
+        "Breadth-First Search",
+        "Greedy",
+        "Tree",
+        "Binary Tree",
+        "Binary Search",
+        "Database",
+        "Matrix",
+        "Two Pointers",
+        "Bit Manipulation",
+        "Stack",
+        "Design",
+        "Heap (Priority Queue)",
+        "Backtracking",
+        "Graph",
+        "Linked List",
+        "Simulation",
+        "Prefix Sum",
+        "Sliding Window",
+        "Counting",
+        "Union Find",
+        "Recursion",
+        "Binary Search Tree",
+        "Divide and Conquer",
+        "Trie",
+        "Monotonic Stack",
+        "Ordered Set",
+        "Queue",
+        "Memoization",
+        "Geometry",
+        "Bitmask",
+        "Enumeration",
+        "Topological Sort",
+        "Segment Tree",
+        "Game Theory",
+        "Hash Function",
+        "Data Stream",
+        "Binary Indexed Tree",
+        "String Matching",
+        "Interactive",
+        "Shortest Path",
+        "Combinatorics",
+        "Rolling Hash",
+        "Randomized",
+        "Merge Sort",
+        "Monotonic Queue",
+        "Doubly-Linked List",
+        "Quickselect",
+        "Number Theory",
+        "Iterator",
+        "Brainteaser",
+        "Probability and Statistics",
+        "Concurrency",
+        "Bucket Sort",
+        "Counting Sort",
+        "Suffix Array",
+        "Minimum Spanning Tree",
+        "Line Sweep",
+        "Shell",
+        "Reservoir Sampling",
+        "Eulerian Circuit",
+        "Strongly Connected Component",
+        "Rejection Sampling",
+        "Radix Sort",
+        "Biconnected Component",
+    }
 }
