886ebe30d1439e210fdfd127b3e781357fe0c8d7
raw34 <raw34@sina.com>
2022-01-21 22:41:23 +0800

    update

--- solution/0001/twoSum.go
+++ solution/0001/twoSum.go
@@ -3,16 +3,15 @@ package _0001
 func twoSum(nums []int, target int) []int {
     n := len(nums)
     hash := map[int]int{}
-    for i := 0; i < n; i++ {
-        hash[nums[i]] = i
-    }
-
     res := make([]int, 0)
-    for i := n - 1; i >= 0; i-- {
-        num := target - nums[i]
-        if j, ok := hash[num]; ok && i != j {
-            res = []int{i, j}
+    for i := 0; i < n; i++ {
+        curr := nums[i]
+        prev := target - curr
+        if j, ok := hash[prev]; ok && i != j {
+            res = []int{j, i}
+            break
         }
+        hash[curr] = i
     }
 
     return res
