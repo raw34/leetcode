d15855453db6673074601864e17216c78c2027e6
raw34 <raw34@sina.com>
2022-01-22 16:13:54 +0800

    update

--- solution/0146/LRUCache.go
+++ solution/0146/LRUCache.go
@@ -1,54 +1,93 @@
 package _0146
 
+import "fmt"
+
+type DListNode struct {
+    Key  int
+    Val  int
+    Prev *DListNode
+    Next *DListNode
+}
+
+type DLinkedList struct {
+    head *DListNode
+    tail *DListNode
+    size int
+}
+
+func (this *DLinkedList) AddToHead(node *DListNode) {
+    node.Prev = this.head
+    node.Next = this.head.Next
+    this.head.Next.Prev = node
+    this.head.Next = node
+    this.size++
+}
+
+func (this *DLinkedList) RemoveNode(node *DListNode) {
+    node.Prev.Next = node.Next
+    node.Next.Prev = node.Prev
+    this.size--
+}
+
+func (this *DLinkedList) RemoveTail() *DListNode {
+    node := this.tail.Prev
+    this.RemoveNode(node)
+    return node
+}
+
+func (this *DLinkedList) Display() {
+    curr := this.head
+    for curr != nil {
+        fmt.Print(curr, " ")
+        curr = curr.Next
+    }
+    fmt.Println()
+}
+
 type LRUCache struct {
     capacity int
-    hash     map[int]int
-    queue    []int
+    hash     map[int]*DListNode
+    list     *DLinkedList
 }
 
 func Constructor(capacity int) LRUCache {
+    head := &DListNode{}
+    tail := &DListNode{}
+    head.Next = tail
+    tail.Prev = head
+
     return LRUCache{
         capacity: capacity,
-        hash:     map[int]int{},
-        queue:    make([]int, capacity),
+        hash:     map[int]*DListNode{},
+        list:     &DLinkedList{head, tail, 0},
     }
 }
 
 func (this *LRUCache) Get(key int) int {
-    if _, ok := this.hash[key]; !ok {
+    node, ok := this.hash[key]
+    if !ok {
         // 未命中缓存，直接返回
         return -1
     }
-    value := this.hash[key]
-    // 将元素重新缓存
-    this.Put(key, value)
+    // 更新缓存热度
+    this.list.RemoveNode(node)
+    this.list.AddToHead(node)
     // 返回元素
-    return value
+    return node.Val
 }
 
 func (this *LRUCache) Put(key int, value int) {
-    // 清理缓存
-    this.cleanup(key)
-    // 插入新元素
-    this.hash[key] = value
-}
-
-func (this *LRUCache) cleanup(key int) {
-    if _, ok := this.hash[key]; ok {
-        // 缓存命中，找到并删除命中的key
-        for i := 0; i < len(this.queue); i++ {
-            if this.queue[i] == key {
-                this.queue = append(this.queue[:i], this.queue[i+1:]...)
-            }
-        }
-    } else if len(this.queue) == this.capacity {
-        // 缓存空间不够，获取最少访问的key
-        keyOld := this.queue[0]
-        // 将最少访问的key出队
-        this.queue = this.queue[1:]
-        // 清理被出队的key的缓存
-        delete(this.hash, keyOld)
+    if node, ok := this.hash[key]; ok {
+        // 如果key存在，删除元素
+        this.list.RemoveNode(node)
     }
-    // 将元素key入队
-    this.queue = append(this.queue, key)
+    if this.list.size == this.capacity {
+        // 如果空间已满，清理缓存
+        removed := this.list.RemoveTail()
+        delete(this.hash, removed.Key)
+    }
+    // 添加缓存和度
+    node := &DListNode{Key: key, Val: value}
+    this.hash[key] = node
+    this.list.AddToHead(node)
 }
