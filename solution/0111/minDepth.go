c37d31891709efa6de84b79542150f812830e874
raw34 <raw34@sina.com>
2022-01-24 17:11:23 +0800

    update

--- solution/0111/minDepth.go
+++ solution/0111/minDepth.go
@@ -6,6 +6,27 @@ import (
 )
 
 func minDepth(root *runtime.TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    if root.Left == nil && root.Right != nil {
+        return minDepth(root.Right) + 1
+    }
+    if root.Left != nil && root.Right == nil {
+        return minDepth(root.Left) + 1
+    }
+
+    return min(minDepth(root.Left), minDepth(root.Right)) + 1
+}
+
+func min(a, b int) int {
+    if a > b {
+        return b
+    }
+    return a
+}
+
+func minDepth2(root *runtime.TreeNode) int {
     min := func(a, b int) int {
         if a > b {
             return b
