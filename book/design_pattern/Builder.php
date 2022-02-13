c3daf7fc750406adfbc5c5dba979c72a4125188e
raw34 <raw34@sina.com>
2022-02-13 20:15:50 +0800

    update

---
+++ book/design_pattern/Builder.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Book\DesignPattern\Builder;
+
+class Burger
+{
+    public int $size;
+
+    public bool $cheese = false;
+    public bool $pepperoni = false;
+    public bool $lettuce = false;
+    public bool $tomato = false;
+
+    public function __construct(BurgerBuilder $builder)
+    {
+        $this->size = $builder->size;
+        $this->cheese = $builder->cheese;
+        $this->pepperoni = $builder->pepperoni;
+        $this->lettuce = $builder->lettuce;
+        $this->tomato = $builder->tomato;
+    }
+}
+
+class BurgerBuilder
+{
+    public int $size;
+
+    public bool $cheese = false;
+    public bool $pepperoni = false;
+    public bool $lettuce = false;
+    public bool $tomato = false;
+
+    public function __construct(int $size)
+    {
+        $this->size = $size;
+    }
+
+    public function addPepperoni()
+    {
+        $this->pepperoni = true;
+        return $this;
+    }
+
+    public function addLettuce()
+    {
+        $this->lettuce = true;
+        return $this;
+    }
+
+    public function addCheese()
+    {
+        $this->cheese = true;
+        return $this;
+    }
+
+    public function addTomato()
+    {
+        $this->tomato = true;
+        return $this;
+    }
+
+    public function build(): Burger
+    {
+        return new Burger($this);
+    }
+}
+
+$burger = (new BurgerBuilder(14))
+    ->addPepperoni()
+    ->addLettuce()
+    ->addTomato()
+    ->build();
