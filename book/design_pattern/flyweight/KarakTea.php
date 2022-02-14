178e38d320de71fd46410fa5c71e05d73b2791d6
raw34 <raw34@sina.com>
2022-02-14 14:34:21 +0800

    update

---
+++ book/design_pattern/flyweight/KarakTea.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace book\design_pattern\flyweight;
+
+// Anything that will be cached is flyweight.
+// Types of tea here will be flyweights.
+class KarakTea
+{
+}
---
+++ book/design_pattern/flyweight/TeaMaker.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace book\design_pattern\flyweight;
+
+class TeaMaker
+{
+    private array $availableTea = [];
+
+    public function make($preference)
+    {
+        if (empty($this->availableTea[$preference])) {
+            $this->availableTea[$preference] = new KarakTea();
+        }
+
+        return $this->availableTea[$preference];
+    }
+}
